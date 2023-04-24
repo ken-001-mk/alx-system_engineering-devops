88b10a201daa086ffe9c9151532e53e8b10b84f5
ken-001-mk <mwaskenne@gmail.com>
2023-04-24 20:18:46 +0000

    configuration complete

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # create a manifest that kills a process named killmenow
 
-exec { 'pkill':
+exec { 'killmenow':
  command => 'pkill -f killmenow',
  path    => ['/usr/bin', '/usr/sbin'],
- onlyif  => 'pgrep killmenow',
 }
