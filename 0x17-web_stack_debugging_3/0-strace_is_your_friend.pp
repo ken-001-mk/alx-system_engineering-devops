a7b749ebfbade93835757db8d723bc5ca4d274e5
ken-001-mk <mwaskenne@gmail.com>
2023-06-07 18:27:09 +0000

    fixing WP

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixing all the termintion of phpp.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
